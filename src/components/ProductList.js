fc1ded0fd3fc9085bf724adcfec933d9a8dbd33d
Rocco P <roccopan@gmail.com>
2021-03-23 15:28:47 -0400

    Added "antd" prestyled components

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Product from 'components/Product'
+import { Pagination } from 'antd'
+import 'antd/dist/antd.css'
 
 const ProductList = ({products}) => {
   
   const allProducts = products.map((prod, i) => <Product key={prod.id} data={prod} style={(i%2) ? {transform: `rotate(180deg)`} : {}} />)
+  // const [pageWereOn, setPageWereOn] = useState(1)
 
   return (
 
@@ -18,13 +21,14 @@ const ProductList = ({products}) => {
 
       <nav aria-label="Pagination" className="pagination">
         <p>1-6 of {allProducts.length} products found</p>
-        <ol className="pages">
+        {/* <ol className="pages">
           <li><a href="#" aria-label="Current Page, Page 1" aria-current="true">1</a></li>
           <li><a href="#" aria-label="Page 2">2</a></li>
           <li><a href="#" aria-label="Page 3">3</a></li>
           <li><a href="#" aria-label="Page 4">4</a></li>
           <li><a href="#" aria-label="Page 5">5</a></li>
-        </ol>
+        </ol> */}
+        <Pagination defaultCurrent={1} total={50} defaultPageSize={5} />
       </nav>
     </>
   )
